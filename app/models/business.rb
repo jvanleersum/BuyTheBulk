661a83b7c19a3789c924a7d72e16f0cb4ee9374a
Judith van Leersum <judithvleersum@gmail.com>
2022-05-30 16:51:36 +0200

    Updated validations

--- app/models/business.rb
+++ app/models/business.rb
@@ -2,7 +2,7 @@ class Business < ApplicationRecord
   has_one :user, dependent: :destroy
 
   validates :nickname, presence: true, uniqueness: true, length: { minimum: 4 , maximum: 20 }
-  validates :description, presence: true, length: { minimum: 50 , maximum: 200 }
+  validates :description, presence: true, length: { minimum: 50 , maximum: 350 }
   validates :address, presence: true
   validates :name, presence: true
 end
