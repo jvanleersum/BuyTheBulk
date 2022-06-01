51ad47ae871f4628ed4f8e77134c8f997b299f75
Judith van Leersum <judithvleersum@gmail.com>
2022-06-01 16:06:16 +0200

    updated categories grid to css grid

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :ui_kit ]
 
   def home
-    @categories = 
+    @categories = %w(furniture textiles decoration kitchenware\ &\ dinnerware take\ away pots\ &\ plants outdoor)
   end
 
   def ui
