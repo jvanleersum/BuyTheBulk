5c1ff79cd63cc0ceec50613fc9087455e1ac6678
Judith van Leersum <judithvleersum@gmail.com>
2022-06-01 15:19:44 +0200

    Included basic ActiveRecords search function for specific product

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,7 +1,12 @@
 class OffersController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   def index
-    @offers = policy_scope(Offer).order(deadline: :desc)
+    if params[:query].present?
+      sql_query = 'name ILIKE :query OR description ILIKE :query'
+      @offers = policy_scope(Offer).where(sql_query, query: "%#{params[:query]}%").order(deadline: :desc)
+    else
+      @offers = policy_scope(Offer).order(deadline: :desc)
+    end
   end
 
   def show
