14db0d39d8b78ba41acfb9f0ca87f539eccaf83b
Judith van Leersum <judithvleersum@gmail.com>
2022-06-01 15:33:35 +0200

    Commiting to merge master

---
+++ app/javascript/controllers/select_categories_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ "category", "form" ]
+
+  // connect() {
+  //   console.log("Hello from our first Stimulus controller")
+  // }
+
+  selectCategory() {
+    
+  }
+}
