92a0489dcbba5cc6ef8177e741d5f65f0c5ad569
Judith van Leersum <judithvleersum@gmail.com>
2022-06-02 15:59:20 +0200

    updated color on progress bar and updated amount validation on order

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,5 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :offer
 
-  validates :amount, presence: true, numericality: { only_integer: true, greater_than: 1 }
+  validates :amount, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
