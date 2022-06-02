a14e19be0a7909824059131c5a46bfa7462f9649
David Mendoza <dmendozasommer@gmail.com>
2022-06-02 12:56:00 +0200

    Order Button finally Works

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,6 +15,12 @@ class OrdersController < ApplicationController
     @order = Order.new(order_params)
     authorize @offer
     @order.offer = @offer
+    @order.user = current_user
+    if @order.save!
+      redirect_to order_confirmation_path(@order)
+    else
+      render "offers/show"
+    end
   end
 
   def order_params
