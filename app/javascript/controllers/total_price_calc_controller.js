dfae8e6b2e727805dd3fe0e82918a1c04e3b7aef
Lara Keen <lara.jr.keen@gmail.com>
2022-06-02 16:48:32 +0200

    total price calc is working

---
+++ app/javascript/controllers/total_price_calc_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "stimulus"
+import OccurrenceOrderChunkIdsPlugin from "webpack/lib/optimize/OccurrenceChunkOrderPlugin"
+
+export default class extends Controller {
+  static targets = [ "total", "savings", "amount"]
+  static values = { unitPrice: Number }
+
+  connect() {
+    console.log('Hello, Stimulus!')
+  }
+
+  updatePrice(e) {
+    const total = (this.unitPriceValue * this.amountTarget.value).toFixed(2)
+    this.totalTarget.innerText = `${total}€`
+    // total = (order.amount * offer.initial_price)
+  }
+}
