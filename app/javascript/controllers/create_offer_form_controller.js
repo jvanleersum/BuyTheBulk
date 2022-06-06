f9ee686f8f4068ec3610269ba1dfeae2abaade8b
Judith van Leersum <judithvleersum@gmail.com>
2022-06-06 15:11:19 +0200

    Form redesigned

--- app/javascript/controllers/create_offer_form_controller.js
+++ app/javascript/controllers/create_offer_form_controller.js
@@ -1,91 +1,67 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "back", "next", "dot1", "dot2", "dot3", "form1", "form2", "form3", "submit" ]
+  static targets = [ "back", "next", "dot", "form", "submit" ]
 
   connect() {
     this.currentIndex = 0;
-    console.log(this.backTarget)
-    console.log(this.nextTarget)
-    console.log(this.dot1Target)
-    console.log(this.dot2Target)
-    console.log(this.dot3Target)
-    console.log(this.form1Target)
-    console.log(this.form2Target)
-    console.log(this.form3Target)
+  }
+
+  checkLabels(){
+    // update arrows
+    if (this.currentIndex == this.formTargets.length - 1) {
+      this.submitTarget.removeAttribute("disabled")
+      this.nextTarget.classList.add("hidden")
+    } else {
+      this.nextTarget.classList.remove("hidden")
+    }
+    if (this.currentIndex == 0) {
+      this.backTarget.classList.add("hidden")
+    } else {
+      this.backTarget.classList.remove("hidden")
+    }
+
+    // update dots
+    this.dotTargets.forEach(dot => {
+      if (dot.classList.contains("active")){
+        dot.classList.remove("active")
+        dot.classList.remove("fas")
+        dot.classList.add("far")
+      }
+    })
+    this.dotTargets[this.currentIndex].classList.remove('far')
+    this.dotTargets[this.currentIndex].classList.add('fas')
+    this.dotTargets[this.currentIndex].classList.add('active')
   }
 
   back() {
-    if (this.form2Target.classList.contains("active")) {
-      console.log("back to section 1")
-      this.form1Target.classList.remove("hidden")
-      this.form1Target.classList.add("active")
-      this.form2Target.classList.remove("active")
-      this.form2Target.classList.add("hidden")
-
-      this.backTarget.classList.add("hidden")
-
-      this.dot1Target.classList.remove("far")
-      this.dot1Target.classList.add("active")
-      this.dot1Target.classList.add("fas")
-
-      this.dot2Target.classList.remove("fas")
-      this.dot2Target.classList.remove("active")
-      this.dot2Target.classList.add("far")
-    } else if (this.form3Target.classList.contains("active")) {
-      console.log("back to section 2")
-      this.form2Target.classList.remove("hidden")
-      this.form2Target.classList.add("active")
-      this.form3Target.classList.remove("active")
-      this.form3Target.classList.add("hidden")
-
-      this.nextTarget.classList.remove("hidden")
-
-      this.dot2Target.classList.remove("far")
-      this.dot2Target.classList.add("active")
-      this.dot2Target.classList.add("fas")
-
-      this.dot3Target.classList.remove("fas")
-      this.dot3Target.classList.remove("active")
-      this.dot3Target.classList.add("far")
+    if (this.currentIndex > 0) {
+      this.currentIndex -= 1
+      this.formTargets.forEach(form => {
+        if (!form.classList.contains('hidden')) {
+          form.classList.add('hidden') 
+        }
+      })
+      this.formTargets[this.currentIndex].classList.remove('hidden')
     }
+    this.checkLabels()
   }
 
   next() {
-    if (this.form1Target.classList.contains("active")) {
-      console.log("go to section 2")
-      this.backTarget.classList.remove("hidden")
-      this.form2Target.classList.remove("hidden")
-      this.form2Target.classList.add("active")
-      this.form1Target.classList.remove("active")
-      this.form1Target.classList.add("hidden")
-
-      this.dot2Target.classList.remove("far")
-      this.dot2Target.classList.add("active")
-      this.dot2Target.classList.add("fas")
-
-      this.dot1Target.classList.remove("fas")
-      this.dot1Target.classList.remove("active")
-      this.dot1Target.classList.add("far")
-    } else if (this.form2Target.classList.contains("active")) {
-      console.log("go to section 3")
-      this.form3Target.classList.remove("hidden")
-      this.form3Target.classList.add("active")
-      this.form2Target.classList.remove("active")
-      this.form2Target.classList.add("hidden")
-
-      this.nextTarget.classList.add("hidden")
-
-      this.dot3Target.classList.remove("far")
-      this.dot3Target.classList.add("active")
-      this.dot3Target.classList.add("fas")
-
-      this.dot2Target.classList.remove("fas")
-      this.dot2Target.classList.remove("active")
-      this.dot2Target.classList.add("far")
-
-      this.submitTarget.removeAttribute("disabled")
+    if (this.currentIndex < this.formTargets.length - 1) {
+      this.currentIndex += 1
+      this.formTargets.forEach(form => {
+        if (!form.classList.contains('hidden')) {
+          form.classList.add('hidden') 
+        }
+      })
+      this.formTargets[this.currentIndex].classList.remove('hidden')
     }
+    this.checkLabels()
+  }
+
+  selectDot(e){
+    console.log(e)
   }
 }
 
