4dd90ddc77acceb74e9d458f7934fafe9836ca45
Laura Gil <lauracodesnow@gmail.com>
2022-06-06 10:42:54 +0200

    Fixed the bug, related with turbolinks and external librariess

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,11 +1,13 @@
 import flatpickr from "flatpickr";
 
 const initFlatpickr = () => {
-  console.log('hey')
-  flatpickr(".datepicker", {
-    disableMobile: true,
-    minDate: "today"
-  });
+  const flatpicker = document.getElementById('offer_deadline');
+  if (flatpicker) {
+    flatpickr(".datepicker", {
+      disableMobile: true,
+      minDate: "today"
+    });
+  }
 }
 
 export { initFlatpickr };
